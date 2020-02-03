b7f032cd6066071908313c6587f8522e92393cc0
oliverh100 <52200950+oliverh100@users.noreply.github.com>
2020-02-03 21:30:04 +0000

    Add files via upload
    Final versions

--- main.py
+++ main.py
@@ -1,59 +1,103 @@
 from scipy.integrate import odeint
-import scipy as sci
+import scipy.optimize
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import animation
 
-G = 6.674 * 10 ** - 11  # gravitational constant
+G = 6.67408 * 10 ** - 11  # gravitational constant
+x0 = 127764396  # initial radius of particle
+k = 9
+w0 = 115100000 * x0 ** -1.5  # initial angular velocity of particle
+M = 5.68319 * 10 ** 26  # mass of saturn
+m = 100.0  # mass of particle
+n = 37505676206690400000  # mass of mimas
+z = 185539000  # distance of mimas from saturn
+L = 2.12 * 10 ** 12  # angular momentum of particle
+alpha = (G * M / np.pi * (x0 ** 3)) - (np.sqrt(G * M) * w0) / (np.pi * (x0 ** 1.5))  # angular acceleration of particle
+w1 = np.sqrt((G * M) / (z ** 3))
 
-x0 = 127764000  # initial r for particle
-k = '7over4'
-w0 = 195100000 * x0 ** -1.5
-M = 5.683 * 10 ** 26
-m = 100.0
-n = 3.751 * 10 ** 19
-z = 185539000
-alpha = 1.0
-phi = np.pi
-a = 1.18 * 10 ** 8
-L = 2.12 * 10 ** 12
-
-
-def solver(w, t, G, a, w0, m, M, n, z, alpha, phi, L):
-    x = w[0]
-    v = w[1]
-
-    h = w0 * t + 0.5 * alpha * (t ** 2) - phi
-
-    p1 = 3 * G * M / (a ** 2)
-    p2 = -4 * (L ** 2) / ((a ** 3) * (m ** 2))
-    p3 = (G * n * (a - z * np.cos(h))) / (((a ** 2) + (z ** 2) - 2 * a * z * np.cos(h)) ** 1.5)
-    p4 = a * G * n * (4 * (a ** 2) + (z ** 2) - 8 * a * z * np.cos(h) + 3 * (z ** 2) * np.cos(2 * h))
-    p5 = 2 * ((a ** 2) + (z ** 2) - 2 * a * z * np.cos(h)) ** 2.5
-    p6 = -2 * G * M / (a ** 3)
-    p7 = 3 * (L ** 2) / ((a ** 4) * (m ** 2))
-    p8 = -G * n * (2 * (a ** 2) + (z ** 2) - 4 * a * z * np.cos(h) + 3 * (z ** 2) * ((np.cos(h)) ** 2))
-    p9 = ((a ** 2) + (z ** 2) - 2 * a * z * np.cos(h)) ** 2.5
-
-    acc = p1 + p2 + p3 + (p4 / p5) + x * (p6 + p7 + (p8 / p9))
-
-    return [v, acc]
+x0s = [
+    116882246,
+    89197848,
+    73631201,
+    141592757,
+    100726066,
+    131988485,
+    80486526,
+    105467185,
+    127764396
+]  # array of radii to use
 
 
-time_span = np.linspace(0, 10 ** 5, 10 ** 7)
+def solver(w, t, G, x0, w0, m, M, n, z, alpha, L, w1):
+    x = w[0]  # getting distance for this iteration
+    v = w[1]  # getting velocity for this iteration
 
-solution = odeint(solver, [x0, 1], time_span, args=(G, a, w0, m, M, n, z, alpha, phi, L))
+    h = (w0 - w1) * t + 0.5 * alpha * (t ** 2)
+    p1 = 3 * G * M / (x0 ** 2)
+    p2 = -4 * (L ** 2) / ((x0 ** 3) * (m ** 2))
+    p3 = (G * n * (x0 - z * np.cos(h))) / (((x0 ** 2) + (z ** 2) - 2 * x0 * z * np.cos(h)) ** 1.5)
+    p4 = x0 * G * n * (4 * (x0 ** 2) + (z ** 2) - 8 * x0 * z * np.cos(h) + 3 * (z ** 2) * np.cos(2 * h))
+    p5 = 2 * ((x0 ** 2) + (z ** 2) - 2 * x0 * z * np.cos(h)) ** 2.5
+    p6 = -2 * G * M / (x0 ** 3)
+    p7 = 3 * (L ** 2) / ((x0 ** 4) * (m ** 2))
+    p8 = -G * n * (2 * (x0 ** 2) + (z ** 2) - 4 * x0 * z * np.cos(h) + 3 * (z ** 2) * ((np.cos(h)) ** 2))
+    p9 = ((x0 ** 2) + (z ** 2) - 2 * x0 * z * np.cos(h)) ** 2.5
 
-xs = solution[:, 0]
-vs = solution[:, 1]
+    acceleration = p1 + p2 + p3 + (p4 / p5) + x * (p6 + p7 + (p8 / p9))  # calculation the equation
 
+    return [v, acceleration]
+
+
+def fit_sin(tt, yy):
+    """
+    A function that extracts the equation of the best fitting sine curve to points given
+    """
+
+    # creating empty arrays for the times, and values to fill up
+    tt = np.array(tt)
+    yy = np.array(yy)
+    ff = np.fft.fftfreq(len(tt), (tt[1] - tt[0]))  # assume uniform spacing of peaks
+    Fyy = abs(np.fft.fft(yy))  # does a fast fourier transform to extract peaks
+    # guess values for the equation, which makes the algorithm faster since it doesn't need to iterate much if it's already near
+    guess_freq = abs(ff[np.argmax(Fyy[1:]) + 1])  # excluding the zero frequency "peak", which is related to offset
+    guess_amp = np.std(yy) * 2. ** 0.5
+    guess_offset = np.mean(yy)
+    guess = np.array([guess_amp, 2. * np.pi * guess_freq, 0., guess_offset])
+
+    def sinfunc(t, A, w, p, c):  # produces a general sine equation
+        return A * np.sin(w * t + p) + c
+
+    # uses optimise curve function from scipy to iteratively get the closest approximation to the equation
+    popt, pcov = scipy.optimize.curve_fit(sinfunc, tt, yy, p0=guess)
+    A, w, p, c = popt
+    f = w / (2. * np.pi)
+    fitfunc = lambda t: A * np.sin(w * t + p) + c  # produces the function
+    return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1. / f, "fitfunc": fitfunc,
+            "maxcov": np.max(pcov), "rawres": (guess, popt, pcov)}  # returns all needed values
+
+
+time_span = np.linspace(0, 10 ** 6, 10 ** 6)  # creating an array of all the times being used
+
+for x in x0s:  # looping through all the values of the initial semi - major axis of the particle to be tested
+    solution = odeint(solver, [x, np.sqrt(G * M / x)], time_span, args=(G, x, w0, m, M, n, z, alpha, L, w1))  # using numerical integration to generate a solution
+
+    xs = solution[:, 0]  # extracting the distance part of the solution
+    vs = solution[:, 1]  # extracting the velocity part of the solution
+
+    res = fit_sin(time_span, xs)  # getting the equation of the wave
+    omega = res['omega']  # getting the angular frequency
+    amplitude = res['amp']  # getting the amplitude of the wave
+    period = 2 * np.pi / omega  # finding the period from the angular frequency
+    print(x, period, amplitude)  # printing the radius used, the period and the amplitude
+
+
+# make the graph
 plt.xlabel('time/s')
 plt.ylabel('distance from Saturn/m')
 plt.axvline(x=0.0, color=(0, 0, 0))
 plt.axhline(y=0.0, color=(0, 0, 0))
 plt.title('Distance a ring particle gets from Saturn during orbit')
-plt.plot(xs, vs, label='orbit', color='blue')
-
-#  plt.savefig('saturn_k={}.png'.format(k))
+plt.plot(time_span, xs, label='orbit', color='blue')
+plt.legend(loc='best')
 plt.show()
+
---
+++ phase.py
@@ -0,0 +1,74 @@
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import numpy as np
+
+G = 6.67408 * 10 ** - 11  # gravitational constant
+x0 = 127764396  # initial radius of particle
+w0 = 115100000 * x0 ** -1.5  # initial angular velocity of particle
+M = 5.68319 * 10 ** 26  # mass of saturn
+m = 100.0  # mass of particle
+n = 37505676206690400000  # mass of mimas
+z = 185539000  # distance of mimas from saturn
+L = 2.12 * 10 ** 12  # angular momentum of particle
+alpha = (G * M / np.pi * (x0 ** 3)) - (np.sqrt(G * M) * w0) / (np.pi * (x0 ** 1.5))  # angular acceleration of particle
+w1 = np.sqrt((G * M) / (z ** 3))
+
+
+def solver(w, t, G, x0, w0, m, M, n, z, alpha, L, w1):
+    x = w[0]  # getting distance for this iteration
+    v = w[1]  # getting velocity for this iteration
+
+    h = (w0 - w1) * t + 0.5 * alpha * (t ** 2)
+    p1 = 3 * G * M / (x0 ** 2)
+    p2 = -4 * (L ** 2) / ((x0 ** 3) * (m ** 2))
+    p3 = (G * n * (x0 - z * np.cos(h))) / (((x0 ** 2) + (z ** 2) - 2 * x0 * z * np.cos(h)) ** 1.5)
+    p4 = x0 * G * n * (4 * (x0 ** 2) + (z ** 2) - 8 * x0 * z * np.cos(h) + 3 * (z ** 2) * np.cos(2 * h))
+    p5 = 2 * ((x0 ** 2) + (z ** 2) - 2 * x0 * z * np.cos(h)) ** 2.5
+    p6 = -2 * G * M / (x0 ** 3)
+    p7 = 3 * (L ** 2) / ((x0 ** 4) * (m ** 2))
+    p8 = -G * n * (2 * (x0 ** 2) + (z ** 2) - 4 * x0 * z * np.cos(h) + 3 * (z ** 2) * ((np.cos(h)) ** 2))
+    p9 = ((x0 ** 2) + (z ** 2) - 2 * x0 * z * np.cos(h)) ** 2.5
+
+    acceleration = p1 + p2 + p3 + (p4 / p5) + x * (p6 + p7 + (p8 / p9))  # calculation the equation
+
+    return [v, acceleration]
+
+
+time_span = np.linspace(0, 10 ** 6, 10 ** 6)  # creating an array of all the times being used
+
+solution = odeint(solver, [x0, np.sqrt(G * M / x0)], time_span, args=(G, x0, w0, m, M, n, z, alpha, L, w1))  # using numerical integration to generate a solution
+
+xs = solution[:, 0]  # extracting the distance part of the solution
+vs = solution[:, 1]  # extracting the velocity part of the solution
+
+y1 = np.linspace(-5 * 10 ** 10, 5 * 10 ** 10, 50)  # creates an array for what vectors will be calculated
+y2 = np.linspace(-10 ** 7, 10 ** 7, 50)  # creates an array for what vectors will be calculated
+
+Y1, Y2 = np.meshgrid(y1, y2)  # creates an meshgrid the vectors
+
+t = 0  # gives the initial for the phase portrait
+
+u, v = np.zeros(Y1.shape), np.zeros(Y2.shape)  # creates zero arrays with the same size as the vector arrays
+
+NI, NJ = Y1.shape  # gives the dimensions of the Y1 array to new variables
+
+for i in range(NI):  # looping through both dimension to calculate the vectors
+    for j in range(NJ):
+        # calculating the vector for a given position and time
+        x = Y1[i, j]
+        y = Y2[i, j]
+        yprime = solver([x, y], t, G, x0, w0, m, M, n, z, alpha, L, w1)
+        u[i, j] = yprime[0]
+        v[i, j] = yprime[1]
+
+for y20 in np.linspace(0, 10 ** 7, 5):  # looping through predefined values to create curves for
+    tspan = np.linspace(0, 10 ** 7, 200)
+    y0 = [0.0, y20]
+    ys = odeint(solver, y0, time_span, args=(G, x0, w0, m, M, n, z, alpha, L, w1))  # solving the equations using numerical integration
+    plt.plot(ys[:, 0], ys[:, 1], 'b-')  # plotting path
+
+#  make the plot
+Q = plt.quiver(Y1, Y2, u, v, color='r')
+plt.xlabel('x')
+plt.ylabel('dx/dt')
+plt.show()
